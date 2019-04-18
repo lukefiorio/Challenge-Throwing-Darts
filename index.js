0108ecb7bf7fb3f41ef7bf807c448713c8d3f62a
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-18 10:30:04 -1000

    working solution & partial testing

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 function scoreCalculator(arr) {
-  // do work here
+  // not an array
+  if (!Array.isArray(arr)) {
+    throw new Error('Not an array!');
+  }
+  // not every element of array is a number
+  if (
+    !arr.every(function(elem) {
+      return typeof elem === 'number';
+    })
+  ) {
+    throw new Error('Needs to be an array of numbers!');
+  }
+  // radius scores outside of valid range
+  if (Math.max.apply(null, arr) > 20 || Math.min.apply(null, arr) < 1) {
+    throw new Error('Missed the dartboard! (invalid radius)');
+  }
+
+  if (arr.length === 0) {
+    return -1;
+  }
+
+  function radiusToScore(prev, cur) {
+    let curScore = 0;
+    if (cur >= 5 && cur <= 10) {
+      curScore = 5;
+    } else if (cur >= 1 && cur < 5) {
+      curScore = 10;
+    }
+    return (prev += curScore);
+  }
+
+  let score = arr.reduce(radiusToScore, 0);
+  let bonus = 0;
+
+  // determine whether bonus should be awarded
+  if (
+    arr.every(function(elem) {
+      return elem < 5;
+    })
+  ) {
+    bonus = 100;
+  }
+  return score + bonus;
 }
 
 module.exports = scoreCalculator;
--- test/spec.js
+++ test/spec.js
@@ -1,1 +1,34 @@
 // write tests here
+//const chai = require('./../node_modules/chai/chai.js');
+const chai = require('chai');
+const expect = chai.expect;
+const assert = chai.assert;
+const should = chai.should();
+var radiusScores = require('../index.js');
+
+describe('Input', function() {
+  it('should throw an error if radius scores are not in an array', function() {
+    expect(function() {
+      radiusScores('2, 4, 6, 8');
+    }).to.throw('Not an array!');
+  });
+  it('should throw an error if radius scores are not all be numbers', function() {
+    expect(function() {
+      radiusScores([2, 4, '6', 8]);
+    }).to.throw('Needs to be an array of numbers!');
+  });
+  it('should throw an error if radius scores outside valid range', function() {
+    expect(function() {
+      radiusScores([3, 7, 34]);
+    }).to.throw('Missed the dartboard! (invalid radius)');
+  });
+});
+
+describe('Output', function() {
+  it('should return -1 if empty collection', function() {
+    let results = radiusScores([]);
+    expect(results).to.equal(-1);
+  });
+  // add test to make sure scores added correctly
+  // add test to make sure bonus awarded correctly
+});
