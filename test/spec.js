4e5981e2f7e03cceb41f1412a5c1c7aac1d60979
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-19 11:01:06 -1000

    finished testing

--- test/spec.js
+++ test/spec.js
@@ -12,7 +12,7 @@ describe('Input', function() {
       radiusScores('2, 4, 6, 8');
     }).to.throw('Not an array!');
   });
-  it('should throw an error if radius scores are not all be numbers', function() {
+  it('should throw an error if radius scores are not all numbers', function() {
     expect(function() {
       radiusScores([2, 4, '6', 8]);
     }).to.throw('Needs to be an array of numbers!');
@@ -26,8 +26,21 @@ describe('Input', function() {
 
 describe('Output', function() {
   it('should return -1 if empty collection', function() {
-    let results = radiusScores([]);
+    let input = [];
+    let results = radiusScores(input);
     expect(results).to.equal(-1);
+  });
+
+  it('should add scores correctly', function() {
+    let input = [3, 7, 9, 2, 11];
+    let results = radiusScores(input);
+    expect(results).to.equal(30);
+  });
+
+  it('should award 100-point bonus if all radius <5', function() {
+    let input = [3, 4, 1, 2];
+    let results = radiusScores(input);
+    expect(results).to.equal(140);
   });
   // add test to make sure scores added correctly
   // add test to make sure bonus awarded correctly
